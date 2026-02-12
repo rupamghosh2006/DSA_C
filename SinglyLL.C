aec8c87e4b2ce3f7abd06f4b6f5aa7f48ff842e2
rupamghosh2006 <rupamgh2006@gmail.com>
2026-02-13 01:57:37 +0530

    Implement InsertAfterNode function and update menu for node insertion

--- SinglyLL.C
+++ SinglyLL.C
@@ -44,6 +44,43 @@ struct node* InsertAtEnd(struct node* Head, int data){
     return Head;
 }
 
+struct node* InsertAfterNode(struct node* Head, int data, int Location){
+
+    if(Head == NULL){
+        printf("Location not found, insertion not possible.\n");
+        return Head;
+    }
+
+    struct node* newNode = (struct node*)malloc(sizeof(struct node));
+
+    if(newNode == NULL){
+        printf("Memory allocation failed\n");
+        exit(1);
+    }
+
+    newNode->data = data;
+
+    struct node* Mover = Head;
+
+    while(Mover != NULL){
+
+        if(Mover->data == Location){
+
+            newNode->next = Mover->next;
+            Mover->next = newNode;
+
+            return Head;   
+        }
+
+        Mover = Mover->next;
+    }
+
+    printf("Location not found, insertion not possible.\n");
+    free(newNode);  
+
+    return Head;
+}
+
 void displayList(struct node* Head){
 
     if(Head == NULL){
@@ -68,7 +105,7 @@ int main(){
     int choice;
 
     while(1){
-        printf("Enter choice:\n1. Display List\n2. Insert at Begin\n3. Insert at End\n4. Exit\n");
+        printf("Enter choice:\n1. Display List\n2. Insert at Begin\n3. Insert at End\n4. Insert after specific location\n5. Exit\n");
         scanf("%d",&choice);
         switch (choice){
         case 1:{
@@ -90,6 +127,15 @@ int main(){
             break;
         }
         case 4:{
+            int location, data;
+            printf("Enter the value of the Element after which you want to insert the node: ");
+            scanf("%d",&location);
+            printf("Enter the value you want to insert: ");
+            scanf("%d",&data);
+            Head = InsertAfterNode(Head,data,location);
+            break;
+        }
+        case 5:{
             exit(0);
             break;  
         }  
