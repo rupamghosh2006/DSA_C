389708ade94323df69d9a3d8350aa295cd717581
rupamghosh2006 <rupamgh2006@gmail.com>
2026-02-13 00:55:42 +0530

    Add .gitignore, README, and initial implementation of Singly Linked List

---
+++ SinglyLL.C
@@ -0,0 +1,102 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node{
+    int data;
+    struct node* next;
+};
+
+struct node* InsertAtBegin(struct node* Head, int data){
+    struct node* newNode = (struct node*)malloc(sizeof(struct node));
+
+    if(newNode == NULL){
+    printf("Memory allocation failed\n");
+    exit(1);
+    }
+
+    newNode->data = data;
+    newNode->next = Head;
+
+    return newNode;
+}
+
+struct node* InsertAtEnd(struct node* Head, int data){
+    struct node* newNode = (struct node*)malloc(sizeof(struct node));
+
+    if(newNode == NULL){
+    printf("Memory allocation failed\n");
+    exit(1);
+    }
+
+    newNode->data = data;
+    newNode->next = NULL;
+
+     if(Head == NULL){
+        return newNode;
+    }
+
+    struct node* Mover = Head;
+    while(Mover->next != NULL){
+        Mover = Mover->next;
+    }
+    Mover->next = newNode;
+
+    return Head;
+}
+
+void displayList(struct node* Head){
+
+    if(Head == NULL){
+        printf("List is Empty\n");
+        return;
+    }
+
+    struct node* Mover = Head;
+
+    printf("Head->");
+    
+    while(Mover != NULL){
+        printf("%d->",Mover->data);
+        Mover = Mover->next;
+    }
+    
+    printf("NULL\n");
+}
+
+int main(){
+    struct node* Head = NULL;
+    int choice;
+
+    while(1){
+        printf("Enter choice:\n1. Display List\n2. Insert at Begin\n3. Insert at End\n4. Exit\n");
+        scanf("%d",&choice);
+        switch (choice){
+        case 1:{
+            displayList(Head);
+            break;
+        }
+        case 2:{
+            int data;
+            printf("Enter the value you want to insert at the Beginning: ");
+            scanf("%d",&data);
+            Head = InsertAtBegin(Head,data);
+            break;
+        }
+        case 3:{
+            int data;
+            printf("Enter the value you want to insert at the End: ");
+            scanf("%d",&data);
+            Head = InsertAtEnd(Head,data);
+            break;
+        }
+        case 4:{
+            exit(0);
+            break;  
+        }  
+        default:
+            printf("Invalid choice\n");
+            break;
+        }
+    }
+    return 0;
+}
